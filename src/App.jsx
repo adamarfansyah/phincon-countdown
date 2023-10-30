79c0771b12de131052faf2545fa72711d879fdf5
adamarfansyah <adam.arfansyah@phincon.com>
2023-10-30 16:52:50 +0700

    feat(FSB3-5): bugfixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSearchParams } from "react-router-dom";
 
 function App() {
   const navigate = useNavigate();
-  const [setSearchParams] = useSearchParams();
   const date = "2024-07-30T00:00:00";
-  const paramsDate = setSearchParams(date);
 
   useEffect(() => {
     if (date) {
-      navigate(`/countdown?date=${date}`, { state: { date: paramsDate } });
+      navigate(`/countdown?date=${date}`, { state: { date } });
     }
   }, []);
 
